b8523d27a11000afd786b10cf7c1c4612afb6ae0
Hugh Brown <hughdbrown@yahoo.com>
2024-11-01 16:16:49 -0600

    Fix up testing code

--- discount_email/test/test_discount_email.py
+++ discount_email/test/test_discount_email.py
@@ -2,9 +2,9 @@ import pytest
 
 from discount_email.src.discount import is_discount_request_ai
 
-@pytest.fixture
-def manning():
-    return """
+# Email text
+
+manning_text = """
 9 HOURS LEFT!
 
 Save half on all print books, including MEAP preorders.
@@ -16,10 +16,7 @@ Offer ends midnight PT tonight. Only at manning.com.
 The witching hour is soon! In just a few hours we’ll roll over to another special sale!
 """
 
-
-@pytest.fixture
-def udacity():
-    return """
+udacity_text = """
 Time is running out!
 
 This offer isn't a trick—get 40% off before it's gone
@@ -27,37 +24,20 @@ This offer isn't a trick—get 40% off before it's gone
 Use code TREAT40 at checkout to unlock 40% off your Udacity subscription. Offer ends Sunday, November 3rd. Don't be left with an outdated skillset.
 """
 
-
-@pytest.fixture
-def oreilly1():
-    return """
+oreilly1_text = """
 Hurry. Because before you know it this deal will be *poof* gone.
 
 Jump on this deal by midnight on October 31 to lock in a year at 40% off.
 
- 
 Use code FLASHSALE2024
 Expires October 31
 """
 
-
-@pytest.fixture
-def oreilly2():
-    return f"""
-Hurry. Because before you know it this deal will be *poof* gone.
-
-Jump on this deal by midnight on October 31 to lock in a year at 40% off.
-
-Use code FLASHSALE2024
-Expires October 31
-
+oreilly2_text = """
 This offer is nontransferable and expires on October 31, 2024, at 11:59pm PT. This offer is for individual memberships only. Team (multiuser) and enterprise accounts are not eligible. The US$299/year offer is valid for the first year of your membership. Memberships renew at the then-current price (currently US$499/year) unless you cancel in accordance with our Membership Agreement. Tax not included.
 """
 
-
-@pytest.fixture
-def fullstack_io():
-    return """
+fullstack_io_text = """
 Louis is a Tech Lead with a decade of experience in startups and consulting. He’s built GenAI applications for international organizations (UNESCO, Singapore Government, etc.) and private companies (Renault, Private Equity Funds).
 
 And now that you’ve stayed with me in my agony of writing this email, I have a Gift, especially for you:
@@ -67,17 +47,11 @@ You can check out the entire first 5 lessons of the course for free!
 If you’re hooked on this newfound enlightenment, don’t forget to check out Responsive LLM Applications with Server-Sent Events with Louis Sanna. (Currently on sale)
 """
 
-
-@pytest.fixture
-def ama_build():
-    return """
+ama_build_text = """
 What have you built? Send a photo and a short description to AMA’s social media manager, Lee Ray, for possible inclusion on the I Fly AMA Facebook page. And don’t forget the October issue of Model Aviation is dedicated to building and the AMA Plans Service is offering 20% off of all orders until October 31! Additional resources can be found at www.modelaircraft.org/IBUILDAMA.
 """
 
-
-@pytest.fixture
-def trading1():
-    return """
+trading1_text = """
 Right now, this revolutionary tool is running in demo mode, but it’s moving into live trading in 2025. Ready to join the first wave of fully automated trading? Make sure you’re on the waiting list— Premium subscribers get first dibs on entering their names on the list when it opens up.
 
 Join Premium Today!
@@ -85,29 +59,17 @@ Save 25% with code MONSTER25
 Hit refresh on your trading game. This is trading as it should be
 """
 
-@pytest.fixture
-def trading2():
-    return """
-Right now, this revolutionary tool is running in demo mode, but it’s moving into live trading in 2025. Ready to join the first wave of fully automated trading? Make sure you’re on the waiting list— Premium subscribers get first dibs on entering their names on the list when it opens up.
 
-Join Premium Today!
-Save 25% with code MONSTER25
-Hit refresh on your trading game. This is trading as it should be
-
+trading2_text = """
 P.S. Spots on the waitlist are limited, and first come first served. Don’t miss your chance—join premium and be ready for the next round.
 """
 
-
-@pytest.fixture
-def fandango1():
-    return """
+fandango1_text = """
 Buy movie tickets through your Fandango account on fandango.com or the Fandango app or movietickets.com, and receive 125 Fandango FanRewardsPoints for each movie ticket. All or at least some portion of the purchase for each ticket must be paid using a credit or debit card, PayPal, credit in your Fandango account from a prior exchanged purchase or a Fandango gift card (i.e., if you use a promo code then only part of that purchase can be paid for using the promo code) to qualify for FanRewards Points. Movie tickets must be for a movie with a showtime starting before 11:59pm PT on the last day of the promotion period. When you receive 500 FanRewards Points, you will receive a $5.00 Discount Reward which you will need to convert into a Discount Promo Code for use on a qualifying purchase on http://www.fandango.com (which can be used on http://www.movietickets.com) or http://www.FandangoAtHome.com.
 """
 
 
-@pytest.fixture
-def matt_harrison1():
-    return """
+matt_harrison1_text = """
 Hey Hugh
 
 If interviews still give you the chills, we’ve got a treat for you. For the next 24 hours, Effective Python Interviews is 30% off! 🎉
@@ -144,9 +106,8 @@ P.S. Keep an eye out for more Halloween flash sales
 """
 
 
-@pytest.fixture
-def matt_harrison2():
-    return """
+matt_harrison2_text = """
+
 Hey Hugh ,
 
 It’s Halloween, and we’re kicking off a series of spine-tingling flash sales you won’t want to miss! If the thought of mastering Python gives you chills, we’ve got the perfect antidote for you:
@@ -178,138 +139,128 @@ Matt Harrison
 P.S. Stay tuned for more spooktacular flash sales this week!
 """
 
+subscriber_text = "Latest articles\nIf you’re not a subscriber, here’s what you missed this month."
+
+
+# Fixtures
+
+@pytest.fixture
+def manning():
+    return manning_text
+
+
+@pytest.fixture
+def udacity():
+    return udacity_text
+
+
+@pytest.fixture
+def oreilly1():
+    return oreilly1_text
+
+
+@pytest.fixture
+def oreilly2():
+    return f"{oreilly1_text}\n{oreilly2_text}"
+
+
+@pytest.fixture
+def fullstack_io():
+    return fullstack_io_text
+
+
+@pytest.fixture
+def ama_build():
+    return ama_build_text
+
+
+@pytest.fixture
+def trading1():
+    return trading1_text
+
+
+@pytest.fixture
+def trading2():
+    return trading2_text
+
+
+@pytest.fixture
+def fandango1():
+    return fandango1_text
+
+
+@pytest.fixture
+def matt_harrison1():
+    return matt_harrison1_text
+
+
+@pytest.fixture
+def matt_harrison2():
+    return matt_harrison2_text
+
 
 @pytest.fixture
 def subscriber():
-   return "Latest articles\nIf you’re not a subscriber, here’s what you missed this month."
+   return subscriber_text
 
-def test_subscriber_success(subscriber):
-    try:
-        content = subscriber
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+ 
+# Tests
+
+def test_subscriber_no_discount_request(subscriber):
+    request = is_discount_request_ai(subscriber)
+    assert not request.is_discount
 
 
-def test_matt_harrison1_success(matt_harrison1):
-    try:
-        content = matt_harrison1
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_matt_harrison1_no_discount_request(matt_harrison1):
+    request = is_discount_request_ai(matt_harrison1)
+    assert not request.is_discount
 
 
-def test_matt_harrison2_success(matt_harrison2):
-    try:
-        content = matt_harrison2
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_matt_harrison2_no_discount_request(matt_harrison2):
+    request = is_discount_request_ai(matt_harrison2)
+    assert not request.is_discount
 
 
-def test_fandango1_success(fandango1):
-    try:
-        content = fandango1
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_fandango1_no_discount_request(fandango1):
+    request = is_discount_request_ai(fandango1)
+    assert not request.is_discount
 
 
-def test_trading1(trading1):
-    try:
-        content = trading1
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_trading1_no_discount_request(trading1):
+    request = is_discount_request_ai(trading1)
+    assert not request.is_discount
 
 
-def test_trading2(trading2):
-    try:
-        content = trading2
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_trading2_no_discount_request(trading2):
+    request = is_discount_request_ai(trading2)
+    assert not request.is_discount
 
 
-def test_ama_build(ama_build):
-    try:
-        content = ama_build
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_ama_build_no_discount_request(ama_build):
+    request = is_discount_request_ai(ama_build)
+    assert not request.is_discount
 
 
-def test_fullstack_io(fullstack_io):
-    try:
-        content = fullstack_io
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_fullstack_io_no_discount_request(fullstack_io):
+    request = is_discount_request_ai(fullstack_io)
+    assert not request.is_discount
 
 
-def test_oreilly1(oreilly1):
-    try:
-        content = oreilly1
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_oreilly1_no_discount_request(oreilly1):
+    request = is_discount_request_ai(oreilly1)
+    assert not request.is_discount
 
 
-def test_oreilly2(oreilly2):
-    try:
-        content = oreilly2
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_oreilly2_no_discount_request(oreilly2):
+    request = is_discount_request_ai(oreilly2)
+    assert not request.is_discount
 
 
-def test_udacity(udacity):
-    try:
-        content = udacity
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_udacity_no_discount_request(udacity):
+    request = is_discount_request_ai(udacity)
+    assert not request.is_discount
 
 
-def test_manning(manning):
-    try:
-        content = manning
-        request = is_discount_request_ai(content)
-    except Exception as exc:
-        print(f"Exception: {exc}")
-        assert False
-    else:
-        assert not request.is_discount
+def test_manning_no_discount_request(manning):
+    request = is_discount_request_ai(manning)
+    assert not request.is_discount
